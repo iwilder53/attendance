7dfde6d90e593af56629c9c213fd05c0818850dc
Ichin Wilder <yashbgdi@gmail.com>
2024-03-11 23:48:53 +0530

    commit

--- backend/src/courses/course/course.routes.ts
+++ backend/src/courses/course/course.routes.ts
@@ -1,8 +1,14 @@
 import express, { Router } from "express";
-import { addCourse } from "./course.controller";
+import { addCourse, deleteCourse, getCourses } from "./course.controller";
 
 export const CourseRouter: Router = express.Router();
 
 
 // api/course/addcourse
-CourseRouter.post("/addCourse", addCourse);
+CourseRouter.post("/addcourse", addCourse);
+
+// api/course/getcourses
+CourseRouter.get("/getCourses", getCourses);
+
+// api/course/deleteCourse
+CourseRouter.delete("/deletecourse/:id", deleteCourse);
--- backend/src/users/user.routes.ts
+++ backend/src/users/user.routes.ts
@@ -1,12 +1,15 @@
 import express, { Router } from "express";
 import { verifyJwtToken } from "../auth.middleware";
-import { login, getStudents, register, attend, getAttendanceByUser, getAttendanceBySubject, generateAttendance, getStudent } from "./user.controller";
+import { login, getStudents, register, attend, getAttendanceByUser, getAttendanceBySubject, generateAttendance, getStudent, addStudent } from "./user.controller";
 
 export const UserRouter: Router = express.Router();
 
 
 // api/user/login
 UserRouter.post("/login", login);
+
+// api/user/login
+UserRouter.post("/addstudent", addStudent);
 
 // api/user/getstudents
 UserRouter.get("/getstudents", getStudents);
@@ -20,14 +23,14 @@ UserRouter.get("/getstudent/:id", getStudent);
 UserRouter.post("/register", register);
 
 // api/user/markAttendance
-UserRouter.post("/attend", attend);
+UserRouter.post("/attend", verifyJwtToken, attend);
 
 // api/user/generateAttendance
 UserRouter.post("/generateAttendance", generateAttendance);
 
 // api/user/getAttendance
-UserRouter.post("/getattendancebyuser", getAttendanceByUser);
+UserRouter.post("/getattendancebyuser", verifyJwtToken, getAttendanceByUser);
 
 
 // api/user/getAttendanceBySubject
-UserRouter.post("/getattendancebysubject", getAttendanceBySubject);
+UserRouter.post("/getattendancebysubject", verifyJwtToken, getAttendanceBySubject);
